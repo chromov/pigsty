437e9477f8712ad00bc22a90011391449eba9abf
Dmitri Chromov <chromov@gmail.com>
2009-08-24 23:57:58 +0300

    Added rendering functionality

--- lib/controller.php
+++ lib/controller.php
@@ -15,14 +15,90 @@ class Controller {
   private $params = array();
 
   /**
+   * render_options 
+   * The place to strore rendering parameters. Changing through render() method
+   * 
+   * @var array
+   * @access private
+   */
+  private $render_options = array();
+
+  /**
    * __construct 
    * 
    * @param array $params 
    * @access protected
    * @return void
    */
-  function __construct($params) {
+  public function __construct($params) {
     $this->params = $params;
+
+    $this->render_options['module_layout'] = '';
+    $this->render_options['facet_layout'] = '';
+  }
+
+  /**
+   * __destruct 
+   * 
+   * @access public
+   * @return void
+   */
+  public function __destruct() {
+    $base = $_SERVER['DOCUMENT_ROOT']."/";
+    $base = str_replace('//', '/', $base);
+    $base .= "facets/".$this->params['facet']."/modules/".$this->params['module']."/views/".$this->params['controller']."/";
+
+    // prepearing an output
+    $output = $this->prepare_template($base.$this->params['action'].".html.php");
+
+    $module_layout = "html.php";
+    if ($this->render_options['module_layout']) {
+      $module_layout = $this->render_options['module_layout'].".".$module_layout;
+    }
+    $output = $this->prepare_template($base."../../layouts/".$module_layout, $output);
+
+    $facet_layout = "html.php";
+    if ($this->render_options['facet_layout']) {
+      $facet_layout = $this->render_options['facet_layout'].".".$facet_layout;
+    }
+    $output = $this->prepare_template($base."../../../../layouts/".$facet_layout, $output);
+
+    //sending it out
+    echo $output;
+  }
+
+  /**
+   * render 
+   * Gives the ability to change what will be rendered
+   * 
+   * @param array $params 
+   * @access private
+   * @return boolean
+   */
+  private function render($params = array()) {
+    foreach ($params as $key => $value) {
+      $this->render_options[$key] = $value;
+    }
+    return true;
+  }
+
+  /**
+   * prepare_template 
+   * 
+   * @param string $file_path 
+   * @param string $inner_content 
+   * @access private
+   * @return string
+   */
+  private function prepare_template($file_path, $inner_content = "") {
+    if (!file_exists($file_path)) {
+      return "<strong>Can't find template <em>$file_path</em></strong><br/>\n".$inner_content;
+    }
+    ob_start();
+    require($file_path);
+    $result = ob_get_contents();
+    ob_end_clean();
+    return $result;
   }
 
   /* Helper methods */
@@ -39,7 +115,7 @@ class Controller {
    * @return void
    */
   public function link_to($link_text, $route_name, $fixed_params=array(), $query_params=array()) {
-    "<a href=".Router::path_to($route_name, $fixed_params, $query_params).">".$link_text."</a>";
+    "<a href=".Router::load()->path_to($route_name, $fixed_params, $query_params).">".$link_text."</a>";
   }
 
 }
--- test.php
+++ test.php
@@ -1,7 +1,9 @@
 <?php
 
 require_once('./lib/router.php');
+require_once('./lib/controller.php');
 require_once('./lib/inflect.php');
 require_once('./config/routes.php');
-print_r(Router::load()->parse_URI('news/articles/45/dflg', 'get'));
+$params = Router::load()->parse_URI('news/articles/45/dflg', 'get');
+$controller = new Controller($params);
 ?>

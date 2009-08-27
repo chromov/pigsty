4cdf149e1d119cfab6805cde0ac4cc1f9a41ea63
Dmitri Chromov <chromov@gmail.com>
2009-08-27 23:21:16 +0300

    Added get_called_class() function

--- dispatcher.php
+++ dispatcher.php
@@ -1,5 +1,30 @@
 <?php
 
+if(!function_exists('get_called_class')) {
+  function get_called_class() {
+    $bt = debug_backtrace();
+    $l = 0;
+    do {
+        $l++;
+        $lines = file($bt[$l]['file']);
+        $callerLine = $lines[$bt[$l]['line']-1];
+        preg_match('/([a-zA-Z0-9\_]+)::'.$bt[$l]['function'].'/',
+                   $callerLine,
+                   $matches);
+                  
+       if ($matches[1] == 'self') {
+               $line = $bt[$l]['line']-1;
+               while ($line > 0 && strpos($lines[$line], 'class') === false) {
+                   $line--;                  
+               }
+               preg_match('/class[\s]+(.+?)[\s]+/si', $lines[$line], $matches);
+       }
+    }
+    while ($matches[1] == 'parent'  && $matches[1]);
+    return $matches[1];
+  }
+}
+
   array_walk(glob('./lib/*.php'),create_function('$v,$i', 'return require_once($v);')); 
   array_walk(glob('./config/*.php'),create_function('$v,$i', 'return require_once($v);')); 
   array_walk(glob('./models/*.php'),create_function('$v,$i', 'return require_once($v);')); 

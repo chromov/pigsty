504dfc67dbaf91fe459830efe85108d9f293abdf
Dmitri Chromov <chromov@gmail.com>
2009-09-04 23:44:13 +0300

    Added 404 error handler for all exceptions

--- dispatcher.php
+++ dispatcher.php
@@ -29,23 +29,28 @@ if(!function_exists('get_called_class')) {
   array_walk(glob('./config/*.php'),create_function('$v,$i', 'return require_once($v);')); 
   array_walk(glob('./models/*.php'),create_function('$v,$i', 'return require_once($v);')); 
 
-  $__params = Router::load()->parse_URI($_GET['URI__'], strtolower($_SERVER['REQUEST_METHOD']));
-  if ($__params === false) {
-    throw new Exception("No route");
-  }
+  try {
+    $__params = Router::load()->parse_URI($_GET['URI__'], strtolower($_SERVER['REQUEST_METHOD']));
+    if ($__params === false) {
+      throw new Exception("No route");
+    }
 
-  foreach ($_POST as $key => $val) {
-    if (!array_key_exists($key, $__params)) $__params[$key] = $val;
-  }
-  foreach ($_GET as $key => $val) {
-    if (($key != "URI__") && !array_key_exists($key, $__params)) $__params[$key] = $val;
-  }
+    foreach ($_POST as $key => $val) {
+      if (!array_key_exists($key, $__params)) $__params[$key] = $val;
+    }
+    foreach ($_GET as $key => $val) {
+      if (($key != "URI__") && !array_key_exists($key, $__params)) $__params[$key] = $val;
+    }
 
-  require_once("./controllers/root_controller.php");
-  require_once("./controllers/facets/".$__params['facet']."_controller.php");
-  require_once("./facets/".$__params['facet']."/modules/".$__params['module']."/controllers/".$__params['controller']."_controller.php");
-  $controller_name = ucwords($__params['controller'])."Controller";
-  $controller = new $controller_name($__params);
-  call_user_func(array($controller, $__params['action']."_action"));
+    require_once("./controllers/root_controller.php");
+    require_once("./controllers/facets/".$__params['facet']."_controller.php");
+    require_once("./facets/".$__params['facet']."/modules/".$__params['module']."/controllers/".$__params['controller']."_controller.php");
+    $controller_name = ucwords($__params['controller'])."Controller";
+    $controller = new $controller_name($__params);
+    call_user_func(array($controller, $__params['action']."_action"));
+  } catch(Exception $e) {
+    header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+    include('public/404.html.php');
+  }
 
 ?>
---
+++ public/404.html.php
@@ -0,0 +1,4 @@
+<h1>ERROR 404: Page Not Found</h1>
+<? if($e): ?>
+<!-- <?= $e->getMessage() ?> -->
+<? endif ?>

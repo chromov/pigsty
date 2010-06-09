e93f8e48ac1ca1a59f59b9eb2f9624f7ea5d3d3e
Dmitri Chromov <chromov@gmail.com>
2010-06-09 18:48:42 +0300

    Moved dependecies code to the Pigsty object

--- dispatcher.php
+++ dispatcher.php
@@ -1,33 +1,6 @@
 <?php
 
-if(!function_exists('get_called_class')) {
-  function get_called_class() {
-    $bt = debug_backtrace();
-    $l = 0;
-    do {
-        $l++;
-        $lines = file($bt[$l]['file']);
-        $callerLine = $lines[$bt[$l]['line']-1];
-        preg_match('/([a-zA-Z0-9\_]+)::'.$bt[$l]['function'].'/',
-                   $callerLine,
-                   $matches);
-                  
-       if ($matches[1] == 'self') {
-               $line = $bt[$l]['line']-1;
-               while ($line > 0 && strpos($lines[$line], 'class') === false) {
-                   $line--;                  
-               }
-               preg_match('/class[\s]+(.+?)[\s]+/si', $lines[$line], $matches);
-       }
-    }
-    while ($matches[1] == 'parent'  && $matches[1]);
-    return $matches[1];
-  }
-}
-
-  array_walk(glob('./lib/*.php'),create_function('$v,$i', 'return require_once($v);')); 
-  array_walk(glob('./config/*.php'),create_function('$v,$i', 'return require_once($v);')); 
-  array_walk(glob('./models/*.php'),create_function('$v,$i', 'return require_once($v);')); 
+  require_once('lib/pigsty.php');
 
   try {
     $__params = Router::load()->parse_URI($_GET['URI__'], strtolower($_SERVER['REQUEST_METHOD']));

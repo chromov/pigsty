b73c65cf8c00e4cbda8c102dccb579f674c189d9
Dmitri Chromov <chromov@gmail.com>
2010-08-18 16:53:59 +0300

    Show unrelated year label when there are only year

--- lib/date.php
+++ lib/date.php
@@ -14,13 +14,16 @@ class Date {
     $date_arr = date_parse($str_date);
     $output = "";
 
+    $year_type = array('small', 'related');
     if($date_arr['month'] && $date_arr['day']) {
       $output .= $date_arr['day']." ".I18n::get_month_name($date_arr['month'], array('small', 'related'));
     } elseif($date_arr['month']) {
       $output .= I18n::get_month_name($date_arr['month'], array('capital'));
+    } else {
+      $year_type = array('small');
     }
     if($date_arr['year']) {
-      $output .= " ".$date_arr['year']." ".I18n::get_year_sign(array('small', 'related'));
+      $output .= " ".$date_arr['year']." ".I18n::get_year_sign($year_type);
     }
 
     return $output;

7b4249b32c95d465093287707ef0b8234d2b9f4a
Dmitri Chromov <chromov@gmail.com>
2011-07-13 00:02:26 +0300

    Customizable paginator

--- lib/paginate.php
+++ lib/paginate.php
@@ -55,7 +55,9 @@ class Paginate {
    * @access public
    * @return string
    */
-  public function render() {
+  public function render($options = array()) {
+    $default_options = array('prev' => "&lsaquo;", 'next' => "&rsaquo;");
+    $options = array_merge($default_options, $options);
     $links_parts = array();
     $wing = floor($this->window/2);
     
@@ -97,18 +99,18 @@ class Paginate {
       $link_base = "/".$_GET['URI__']."?page=";
     }
 
-    $res = '<div class="pager" style="margin: 15px 0; text-align:center">';
+    $res = '<div class="pager">';
     if($this->current_page > 1) {
-      $res .= '<span class="backward"><a href="'.$link_base.($this->current_page-1).'" title="Назад" style="font-size:16px;">&lsaquo;</a></span>';
+      $res .= '<span class="backward"><a href="'.$link_base.($this->current_page-1).'" title="Назад">'.$options['prev'].'</a></span>';
     }
-    $res .= '<ul style="list-style: none; display: inline; margin: 0">';
+    $res .= '<ul>';
     
     foreach ($links_parts as $index => $part) {
       foreach ($part as $page_num) {
         if($page_num == $this->current_page) {
-          $res .= '<li style="display: inline;padding: 2px 6px; border: 1px solid #aaa" class="current_page">'.$page_num.'</li>';
+          $res .= '<li class="current_page">'.$page_num.'</li>';
         } else {
-          $res .= '<li style="display: inline;"><a href="'.$link_base.$page_num.'" style="padding: 2px 6px; border: 1px solid #eee">'.$page_num.'</a></li>';
+          $res .= '<li><a href="'.$link_base.$page_num.'">'.$page_num.'</a></li>';
         }
       }
       if($index + 1 != sizeof($links_parts)) {
@@ -118,7 +120,7 @@ class Paginate {
 
     $res .= '</ul>';
     if($this->current_page < $this->total) {
-      $res .= '<span class="forward"><a href="'.$link_base.($this->current_page+1).'" title="Вперед" style="font-size:16px;">&rsaquo;</a></span>';
+      $res .= '<span class="forward"><a href="'.$link_base.($this->current_page+1).'" title="Вперед">'.$options['next'].'</a></span>';
     }
     $res .= '</div>';
     return $res;
